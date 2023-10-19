fdad73b9c8cdd067625d17bec97779dc43330f79
gerdos <e.gabor90@gmail.com>
2023-10-19 11:28:05 +0200

    Added comments

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+# initializing a list
 a = [1,2,3]
+# initializing a number
 b = a
 b[1] = "q"
+# printing a variable
 print(a)
--- test2.py
+++ test2.py
@@ -1,4 +1,6 @@
+# Tis is a useful comment
 a = 1
 b = a
+# This is an even more useful comment
 b = 2
 print(a)
